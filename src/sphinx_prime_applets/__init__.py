b71f5ce3bc90151b895980cefd8a1ce4c4371378
Dennis den Ouden-van der Horst <49307154+douden@users.noreply.github.com>
2025-02-21 13:12:20 +0100

    Update __init__.py

--- src/sphinx_prime_applets/__init__.py
+++ src/sphinx_prime_applets/__init__.py
@@ -3,12 +3,45 @@ from urllib.parse import quote
 from docutils import nodes
 from docutils.parsers.rst import directives
 from sphinx.directives.patches import Figure
-
-from utils import parse_options, generate_style
-
+from typing import Optional
 
 DEFAULT_BASE_URL = "https://openla.ewi.tudelft.nl/applet/"
 
+def generate_style(height: Optional[str], width: Optional[str]):
+	'''
+	Given a height and width, generates an inline style that can be used in HTML.
+	'''
+
+	styles = ''
+
+	if height:
+		styles += f'height: {height};'
+
+	if width:
+		styles += f'width: {width};'
+
+	return styles
+
+
+def parse_value(val: str) -> str:
+	'''
+	Parses a string value to a string that can be used in a URL query parameter. This is a hacky way to use boolean in docutils.
+	(For some reason docutils can't parse 'true' or 'True' strings??)
+	'''
+
+	if val == 'enabled':
+		return 'true'
+	elif val == 'disabled':
+		return 'false'
+	else:
+		return str(val)
+
+
+def parse_options(options: dict) -> dict:
+	# Settings keys that are passed along to the applet iframe
+	applet_keys = ['title', 'background', 'autoPlay', 'position', 'isPerspectiveCamera', 'enablePan', 'distance', 'zoom']
+
+	return {key: parse_value(val) for key, val in options.items() if key in applet_keys and val != ''}
 
 class AppletDirective(Figure):
     option_spec = Figure.option_spec.copy()
