c36a1a726b861a237f8ce9875401448d8a1c04b9
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2025-12-03 13:15:52 +0100

    Handle missing GitHub token in get_last_modified_date
    Updated get_last_modified_date to return None if no GitHub token is provided, preventing rate limiting issues. Removed related logging from the setup function.

--- src/sphinx_prime_applets/__init__.py
+++ src/sphinx_prime_applets/__init__.py
@@ -17,6 +17,10 @@ TOKEN = os.getenv("GITHUB_TOKEN")
 DEFAULT_BASE_URL = "https://openla.ewi.tudelft.nl/applet/"
 
 def get_last_modified_date(file_url, token=TOKEN):
+
+    # to avoid rate limiting issues with the GitHub API, only proceed if a token is provided. Otherwise, return None
+    if token is None:
+        return None
     # Extract parts from the GitHub URL
     parts = file_url.split('/')
     owner = parts[3]
@@ -212,10 +216,6 @@ class AppletDirective(MetadataFigure):
         
 def setup(app):
 
-    if TOKEN is None:
-        logger.warning("GITHUB_TOKEN environment variable not set. Rate limiting may occur when fetching last modified dates from GitHub.",color='green')
-    else:
-        logger.info(f"GITHUB_TOKEN {TOKEN} found. Using it to fetch last modified dates from GitHub.",color='green')
     app.setup_extension('sphinx_metadata_figure')
     app.add_config_value("prime_applets_metadata", True, "env")
     app.add_directive("applet", AppletDirective)
